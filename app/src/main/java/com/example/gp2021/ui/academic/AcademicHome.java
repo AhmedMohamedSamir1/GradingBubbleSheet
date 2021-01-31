69bdc1fd782ba84c8c573833f78197f78d955308
ahmed <ahmedasas222@yahoo.com>
2021-02-01 00:32:16 +0200

    commit by Ahmed

--- app/src/main/java/com/example/gp2021/ui/academic/AcademicHome.java
+++ app/src/main/java/com/example/gp2021/ui/academic/AcademicHome.java
@@ -3,6 +3,7 @@ package com.example.gp2021.ui.academic;
 //Test Kelly2
 //Test Kelly2
 //Test Kelly2
+//Test Ahmed
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
