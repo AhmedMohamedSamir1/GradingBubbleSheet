716e034f68db14e052d97a203095252a135ee837
ahmed <ahmedasas222@yahoo.com>
2021-07-03 00:35:10 +0200

    add answer manaually.....

---
+++ app/src/main/java/com/example/gp2021/ui/instructor/examAnsHolder.java
@@ -0,0 +1,9 @@
+package com.example.gp2021.ui.instructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public  class examAnsHolder {
+
+   public static List<String> examAns = new ArrayList<>();
+}
